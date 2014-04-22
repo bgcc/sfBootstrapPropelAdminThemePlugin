e72983792a70b28838ad36a78a56bb407cd6e7ae
19Gerhard85 <gerhard.schranz@gmail.com>
2014-04-22 07:25:34 +0200

    New widget for input groups

---
+++ lib/widget/sfWidgetFormBootstrap3Input.class.php
@@ -0,0 +1,35 @@
+<?php
+class sfWidgetFormBootstrap3Input extends sfWidgetFormInput
+{
+    protected function configure($options = array(), $attributes = array())
+    {
+        parent::configure();
+
+        $this->addOption('prepend');
+        $this->addOption('append');
+    }
+
+    public function render($name, $value = null, $attributes = array(), $errors = array())
+    {
+        $input = parent::render($name, $value, $attributes, $errors);
+
+        $prepend = '';
+        $append = '';
+
+        if ($this->getOption('prepend')) {
+            $prepend = '<span class="input-group-addon">' . $this->getOption('prepend') . '</span>';
+        }
+
+        if ($this->getOption('append')) {
+            $append = '<span class="input-group-addon">' . $this->getOption('append') . '</span>';
+        }
+
+        $html =  '<div class="input-group">';
+        $html .=     $prepend;
+        $html .=     $input;
+        $html .=     $append;
+        $html .= '</div>';
+
+        return $html;
+    }
+}

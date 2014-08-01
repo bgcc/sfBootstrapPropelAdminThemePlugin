c121f26d770040d7137e9e9054a91f3d1d382605
19Gerhard85 <gerhard.schranz@gmail.com>
2014-08-01 13:06:45 +0200

    Load theme.js in module assets

--- data/generator/sfPropelModule/bootstrap/template/templates/_assets.php
+++ data/generator/sfPropelModule/bootstrap/template/templates/_assets.php
@@ -1,3 +1,5 @@
 <?php if (isset($this->params['css']) && ($this->params['css'] !== false)): ?>
-[?php use_stylesheet('<?php echo $this->params['css'] ?>', 'first') ?]
+[?php use_stylesheet('<?php echo $this->params['css'] ?>', 'first'); ?]
 <?php endif; ?>
+
+[?php use_javascript('/sfBootstrapPropelAdminThemePlugin/js/theme.js', 'last'); ?]

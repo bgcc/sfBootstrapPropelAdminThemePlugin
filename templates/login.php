730e55b016d54971cb354eae1ebebe6078c98d63
19Gerhard85 <gerhard.mikysa@gmail.com>
2015-02-17 12:55:18 +0100

    Undo

--- templates/login.php
+++ templates/login.php
@@ -15,7 +15,7 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
     </head>
-    <body class="login">
+    <body>
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-6 col-lg-offset-3 main">

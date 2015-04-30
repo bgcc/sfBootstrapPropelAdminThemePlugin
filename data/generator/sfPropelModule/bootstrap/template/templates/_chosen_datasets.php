023c44252c5f108082b24e41314316100e3255d5
Simon Mitterhofer <Tschebel@users.noreply.github.com>
2015-04-30 14:32:51 +0200

    fix I18N helper not loaded on ajax refresh - closes #3

--- data/generator/sfPropelModule/bootstrap/template/templates/_chosen_datasets.php
+++ data/generator/sfPropelModule/bootstrap/template/templates/_chosen_datasets.php
@@ -1,3 +1,4 @@
+[?php use_helper('I18N') ?]
 <span class="chosen_datasets">
     [?php $count = $helper->countBatchIds() ?]
     [?php echo format_number_choice('[0] no datasets chosen|[1] one dataset chosen|(1,+Inf] %1% datasets chosen', array('%1%' => $count), $count, 'sf_admin') ?]

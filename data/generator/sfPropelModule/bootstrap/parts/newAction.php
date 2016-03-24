46c9d7db55e21774189c7eb3ce5560856aa7e0ff
19Gerhard85 <gerhard.mikysa@gmail.com>
2016-03-24 15:46:16 +0100

    Update newAction.php

--- data/generator/sfPropelModule/bootstrap/parts/newAction.php
+++ data/generator/sfPropelModule/bootstrap/parts/newAction.php
@@ -5,7 +5,7 @@
 
         sfContext::getInstance()->getConfiguration()->loadHelpers('I18N');
         $<?php echo $this->getSingularName() ?> = $this-><?php echo $this->getSingularName() ?>;
-        $this->getResponse()->setTitle( <?php echo $this->getI18NString('edit.title') ?>);
+        $this->getResponse()->setTitle( <?php echo $this->getI18NString('new.title') ?>);
 
         <?php echo isset($this->params['disable_form_customization']) && $this->params['disable_form_customization'] ? '' : $this->getFormCustomization('new'); ?>
     }
